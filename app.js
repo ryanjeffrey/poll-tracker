9eb52e7acdebbe006aced70c7e7392d759041b4b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-02 19:05:53 -0500

    Let state for currentPoll object

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const pastPollsSectionEl = document.getElementById('past-polls-section');
 const newPollFormEl = document.getElementById('new-poll-form');
 
 // let state
+let currentPoll = {
+    question: '',
+    optionA: '',
+    optionB: '',
+    votesA: 0,
+    votesB: 0
+};
 
 // set event listeners 
   // get user input
